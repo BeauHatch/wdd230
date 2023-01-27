d1610e1bf4e1f0157336cdee349779d0b21a8f04
BeauHatch <beaujhatch@gmail.com>
2023-01-27 13:10:12 -0700

    please kill me

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -13,17 +13,24 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 // select the elements to manipulate (output to)
 const datefield = document.querySelector(".date");
-const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
 
 // derive the current date using a date object
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
+
 // long, medium, short options ... try them
 
 datefield.innerHTML = `<em>${fulldate}</em>`;
-datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+
+
+document.querySelector(
+	"#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
+
+
+
+new Date(new Date().setFullYear(new Date().getFullYear() + 1))
+
